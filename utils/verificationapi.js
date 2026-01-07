07343513f21f030d081a6441fa7e34ed0e204875
ajaysharmaofficial <ajays1.img@gmail.com>
2026-01-07 18:24:40 +0530

    change

--- utils/verificationapi.js
+++ utils/verificationapi.js
@@ -182,8 +182,6 @@ exports.bankRequest = async ({ accno, ifsc }) => {
         message: "Bank account number and IFSC are required",
       };
     }
-    console.log("accno", accno);
-    console.log("ifsc", ifsc);
     const token = await getSandboxToken();
 
     const response = await axios.get(
