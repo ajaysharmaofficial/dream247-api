e184de00b04b83a97aca76f404618ae91f2af96e
ajaysharmaofficial <ajays1.img@gmail.com>
2026-01-07 13:10:44 +0530

    chage

--- services/deposit-api-service/services/paymentService.js
+++ services/deposit-api-service/services/paymentService.js
@@ -2346,7 +2346,7 @@ exports.razorPayPaymentVerify = async (req) => {
     const body = razorpay_order_id + "|" + razorpay_payment_id;
 
     const expectedSignature = crypto
-      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .createHmac("sha256", global.constant.RAZORPAY_KEY_SECRET_TEST)
       .update(body)
       .digest("hex");
 
